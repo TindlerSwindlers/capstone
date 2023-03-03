9b38d69393dc76fc32cefd3595b37b5bbe2632d1
wonjunjung <tteen82@gmail.com>
2023-03-02 21:29:40 -0500

    addpost

---
+++ client/components/Post.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+export const Post = (props) => {
+  const { text, likes, imageUrl, comments } = props;
+};
--- client/store/posts.js
+++ client/store/posts.js
@@ -1,31 +1,43 @@
-import axios from "axios";
+import axios from 'axios';
 
 //Action types
 
-const GET_POSTS = "GET_POSTS";
+const GET_POSTS = 'GET_POSTS';
+const ADD_POST = 'ADD_POST';
 
 //Action creators
 
 const _getPosts = (posts) => ({ type: GET_POSTS, posts });
+const _addPost = (post) => ({ type: ADD_POST, post });
 
 //Thunk creators
 
 export const fetchPosts = () => {
   return async (dispatch) => {
     try {
-      const response = await axios.get("/api/posts");
+      const response = await axios.get('/api/posts');
       dispatch(_getPosts(response.data));
     } catch (e) {
-      console.log("Error trying to get all posts in thunk", e);
+      console.log('Error trying to get all posts in thunk', e);
     }
   };
 };
 
+export const addPost = (id, data) => {
+  return async (dispatch) => {
+    const response = await axios.post(`/api/posts/${id}`, data);
+    dispatch(_addPost(response.data));
+    history.push('../');
+  };
+};
+
 //Reducer
-export default posts = (state = {}, action) => {
+export default posts = (state = [], action) => {
   switch (action.type) {
     case GET_POSTS:
       return action.posts;
+    case ADD_POST:
+      return [...state, action.post];
     default:
       return state;
   }
