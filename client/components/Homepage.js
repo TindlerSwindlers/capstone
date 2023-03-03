1d22dbc0b90a28a500acb68b51ea538806b83cbf
Ana Otero <anajova711@gmail.com>
2023-03-02 21:24:25 -0500

    getting posts and rendering

--- client/components/Homepage.js
+++ client/components/Homepage.js
@@ -2,16 +2,10 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchPosts } from "../store/posts";
 
-/**
- * COMPONENT
- */
-export const Homepage = (props) => {
-  // const { username } = props;
+export const Homepage = () => {
   const { posts } = useSelector((state) => state);
   const { auth } = useSelector((state) => state);
   const dispatch = useDispatch();
-
-  console.log("POSTS", posts);
 
   useEffect(() => {
     dispatch(fetchPosts());
@@ -20,24 +14,14 @@ export const Homepage = (props) => {
   return (
     <div>
       <h3>Welcome, {auth.username}</h3>
+      {posts.posts &&
+        posts.posts.map((post) => (
+          <div key={post.id}>
+            <p>{post.text}</p>
+          </div>
+        ))}
     </div>
   );
 };
-
-/**
- * CONTAINER
- */
-// const mapState = (state) => {
-//   return {
-//     username: state.auth.username,
-//     posts: state.posts
-//   };
-// };
-
-// const mapDispatch = (dispatch) => {
-//   return {
-//     getPosts: () => dispatch(fetchPosts()),
-//   };
-// };
 
 export default Homepage;
