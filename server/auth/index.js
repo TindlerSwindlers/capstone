42b2a7e8c3ab295a887c66deb5ed52aa54669663
MATT <mgiambrone17@gmail.com>
2023-03-02 21:11:39 -0500

    user-api-minor-change

--- server/auth/index.js
+++ server/auth/index.js
@@ -24,4 +24,11 @@ router.post('/signup', async (req, res, next) => {
   }
 })
 
+router.get('/me', async (req, res, next) => {
+  try {
+    res.send(await User.findByToken(req.headers.authorization))
+  } catch (ex) {
+    next(ex)
+  }
+})
 
