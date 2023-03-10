447a10be344d0a3ec88100c4f7c85625998376e5
MATT <mgiambrone17@gmail.com>
2023-03-09 20:53:20 -0500

    hello

--- client/store/comments.js
+++ client/store/comments.js
@@ -6,6 +6,7 @@ import { fetchPosts } from "./posts";
 const ADD_COMMENT = "ADD_COMMENT";
 const DELETE_COMMENT = "DELETE_COMMENT";
 const SET_COMMENTS = "SET_COMMENTS";
+const FETCH_PROFILE_COMMENTS = "FETCH_PROFILE_COMMENTS"
 //Action creators
 
 const _addComment = (comment) => ({ type: ADD_COMMENT, comment });
@@ -13,6 +14,17 @@ const _deleteComment = (id) => ({ type: DELETE_COMMENT, id });
 const _setComments = (comments) => ({ type: SET_COMMENTS, comments });
 
 //Thunk creators
+
+export const fetchProfileComments = (id) => {
+  return async(dispatch) => {
+    const res = await axios.get(`/api/users/${id}`)
+    dispatch({ 
+      type: FETCH_PROFILE_COMMENTS,
+      comments: res.data.comments
+    });
+  };
+};
+
 
 export const setComments = (post) => {
   return async (dispatch) => {
