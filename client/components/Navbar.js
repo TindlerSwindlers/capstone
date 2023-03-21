acc90ec7b5d03245313e957bf1a9c8571cb331b6
wonjunjung <tteen82@gmail.com>
2023-03-20 21:04:44 -0400

    editprofileimage

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,40 +2,47 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { logout } from '../store/auth';
+import Avatar from '@mui/material/Avatar';
 
-const Navbar = ({ handleClick, isLoggedIn }) => (
-  <div>
-    <h1>Welcome to Spark!</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
+const Navbar = ({ handleClick, isLoggedIn, auth }) => {
+  console.log(auth);
+  return (
+    <div>
+      <h1>Welcome to Spark!</h1>
+      <nav>
+        {isLoggedIn ? (
+          <div>
+            {/* The navbar will show these links after you log in */}
 
-          <Link to="/home">Home</Link>
-          <Link to="/newpost">New Post</Link>
-          <Link to="/profile">Profile</Link>
-          <Link to="/yourMatches">Your Matches</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-);
+            <Link to="/home">Home</Link>
+            <Link to="/newpost">New Post</Link>
+            <Link to="/yourMatches">Your Matches</Link>
+            <Link to="/profile">
+              <Avatar alt={auth.name} src={auth.imageUrl} />
+            </Link>
+            <a href="#" onClick={handleClick}>
+              Logout
+            </a>
+          </div>
+        ) : (
+          <div>
+            {/* The navbar will show these links before you log in */}
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+        )}
+      </nav>
+      <hr />
+    </div>
+  );
+};
 
 /**
  * CONTAINER
  */
 const mapState = (state) => {
   return {
+    auth: state.auth,
     isLoggedIn: !!state.auth.id,
   };
 };
