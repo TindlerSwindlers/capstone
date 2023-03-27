3f42bdf3f7a30677761f2b6c982d42d9d571ed46
Ana Otero <anajova711@gmail.com>
2023-03-27 14:19:13 -0400

    msg-display

--- client/components/Messages/MessageDisplay.js
+++ client/components/Messages/MessageDisplay.js
@@ -1,50 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-
 const MessageDisplay = ({ messages, auth }) => {
+  let users = [];
+  let userMessages = {};
 
-    let users = [];
-    let userMessages = {};
+  messages
+    .filter((message) => message.userSendingId !== auth.id)
+    .map((message) =>
+      !users.some((e) => e.username === message.userSending.username)
+        ? users.push(message.userSending)
+        : null
+    );
 
-  
-    
-    messages.filter(message => message.userSendingId !== auth.id).map(message => !users.some(e => e.username === message.userSending.username) ? users.push(message.userSending) : null)
-
-    for (let y = 0; y < users.length; y++) {
-      for (let i = 0; i < messages.length; i++) {
-        if (messages[i].userSending.username === users[y].username) {
-           userMessages[messages[i].createdAt] = messages[i].text
-           users["usersMessages"] = userMessages
-
+  for (let y = 0; y < users.length; y++) {
+    for (let i = 0; i < messages.length; i++) {
+      if (messages[i].userSending?.username === users[y].username) {
+        userMessages[messages[i].createdAt] = messages[i].text;
+        users["usersMessages"] = userMessages;
+      }
     }
-   }
- 
-}
+  }
 
-    return (
-        <div>
-            {
-               users.map((user) => {
-                    return (
-                        <div key={user.id}>
-                          <p>{user.name} {user.lastName}</p>
-                           <img src={user.imageUrl}></img>
-                           <br></br><br></br>
-                          Messages: <pre> {JSON.stringify(userMessages, null, 2)} </pre>
-                        </div>
-                    )
-                }) 
-            }
-          
-                <div>
-               
-            </div>
-        </div>
-    )
-    
-}
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <p>
+              {user.name} {user.lastName}
+            </p>
+            <img src={user.imageUrl}></img>
+            <br></br>
+            <br></br>
+            Messages: <pre> {JSON.stringify(userMessages, null, 2)} </pre>
+          </div>
+        );
+      })}
 
+      <div></div>
+    </div>
+  );
+};
 
 export default MessageDisplay;
-
